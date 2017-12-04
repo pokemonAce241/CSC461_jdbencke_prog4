317a7654b3200847d3939ecfdc4ed53620aec960
jdbencke <jdbencke@ncsu.edu>
2017-12-04 18:16:03 -0500

    Update rasterize.js

--- rasterize.js
+++ rasterize.js
@@ -1,57 +1,36 @@
 /* GLOBAL CONSTANTS AND VARIABLES */
 
 /* assignment specific globals */
- var scene;
-  
- var camera;
 
- var renderer;
-
- var terrain;
-
-function initializeScene(){
-    renderer = new THREE.WebGLRenderer({antialias:true});
-    renderer.setClearColor(0x000000,1);
-    
-    canvasWidth = window.innerWidth;
-    canvasHeight = window.innerHeight;
-    
-    renderer.setSize(canvasWidth,canvasHeight);
-    
-    document.getElementById("WebGLCanvas").appendChild(renderer.domElement);
-    
-    scene = new THREE.Scene();
-    
-    camera = new THREE.PerspectiveCamera(45,canvasWidth/canvasHeight,1,100);
-    camera.position.set(0,0,10);
-    camera.lookAt(scene.position);
-    scene.add(camera);
-    
-    var terrainGeometry = new THREE.BoxGeometry(canvasWidth,0.5,0.5);
-    var terrainMaterial = new THREE.MeshBasicMaterial({color:0xFF0000});
-    
-    terrain = new THREE.Mesh(terrainGeometry,terrainMaterial);
-    terrain.position.set(1.5,0.0,4.0);
-    scene.add(terrain);
-}
-
-function animateScene(){
-    
-    requestAnimationFrame(animateScene)
-    
-    render.render(scene,camera);
-    
-    }
 
 
 
 function main() {
 
-    initializeScene();
-    animateScene();
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
     
-   
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth,window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    
+    var terrainGeometry = new THREE.BoxGeometry(window.innerWidth,0.7,0.5);
+    var terrainMaterial = new THREE.MeshBasicMaterial({color: 0x800000});
+    var terrain = new THREE.Mesh(terrainGeometry, terrainMaterial);
+    terrain.position.set(1.5,0.0,4.0);
+  
+    scene.add(terrain);
     
     
+    camera.position.z = 10;
+    
+    var animate = function(){
+     requestAnimationFrame(animate);   
+        
+        
+        renderer.render(scene,camera);
+    }
+    
+    animate();
   
 } // end main
