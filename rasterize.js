e4225ac310724131b7bcf8f84a09f212c42c537a
jdbencke <jdbencke@ncsu.edu>
2017-12-04 16:56:33 -0500

    Update rasterize.js

--- rasterize.js
+++ rasterize.js
@@ -17,7 +17,7 @@ function main() {
     var terrainGeometry = new THREE.BoxGeometry(window.innerWidth,0.1,0.5);
     var terrainMaterial = new THREE.MeshBasicMaterial({color: 0x800000});
     var terrain = new THREE.Mesh(terrainGeometry, terrainMaterial);
-    scene.add(cube);
+    scene.add(terrain);
     
     camera.position.z = 5;
     
