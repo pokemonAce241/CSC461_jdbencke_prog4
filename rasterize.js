fa3a1812ad5b8396ea27eb70e69eb034919556eb
jdbencke <jdbencke@ncsu.edu>
2017-12-05 13:02:02 -0500

    Update rasterize.js

--- rasterize.js
+++ rasterize.js
@@ -11,6 +11,10 @@ function main() {
     var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
     camera.lookAt(scene.position);
     
+    var light = new THREE.PointLight(0xff0000, 1, 100);
+    light.position.set( 50, 50, 50 );
+    scene.add(light);
+    
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth,window.innerHeight);
     document.body.appendChild(renderer.domElement);
