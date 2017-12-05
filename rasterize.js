a02000a226c75a22356c780baee8f1c8be6921b6
jdbencke <jdbencke@ncsu.edu>
2017-12-04 21:25:00 -0500

    Update rasterize.js

--- rasterize.js
+++ rasterize.js
@@ -15,11 +15,26 @@ function main() {
     renderer.setSize(window.innerWidth,window.innerHeight);
     document.body.appendChild(renderer.domElement);
     
-    var terrainGeometry = new THREE.BoxGeometry(window.innerWidth,0.7,0.5);
+    var terrainGeometry = new THREE.BoxGeometry(window.innerWidth,0.7,3.0);
     var terrainMaterial = new THREE.MeshBasicMaterial({color: 0x800000});
     var terrain = new THREE.Mesh(terrainGeometry, terrainMaterial);
-    terrain.position.set(1.5,-4.5,4.0);
+    terrain.position.set(1.5,-4.5,-1.0);
     terrain.rotation.x += 0.2;
+    
+    var cityGeometry = new THREE.BoxGeometry(0.3,0.75,0.75);
+    var cityMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00});
+    
+    var batteryGeometry = new THREE.BoxGeometry(0.5,0.2,0.75);
+    var batteryMaterial = new THREE.MeshBasicMaterial({color: 0xff00});
+    
+    var battery1 = new THREE.Mesh(batteryGeometry,batteryMaterial);
+    battery1.position.set(2.0,-4.5,-2.0);
+    
+    
+    var city1 = new THREE.Mesh(cityGeometry,cityMaterial);
+    var city2 = new THREE.Mesh(cityGeometry,cityMaterial);
+    
+    
   
     scene.add(terrain);
     
