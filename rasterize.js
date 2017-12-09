897ce019ce95cc44c5ad5d83fa1329dd86135b9c
jdbencke <jdbencke@ncsu.edu>
2017-12-09 18:44:26 -0500

    Update rasterize.js

--- rasterize.js
+++ rasterize.js
@@ -41,7 +41,17 @@ function main() {
     renderer.setSize(window.innerWidth,window.innerHeight);
     document.body.appendChild(renderer.domElement);
     
+    var backgroundTexture = new THREE.TextureLoader().load('https://github.ncsu.edu/jdbencke/CSC461_jdbencke_prog4/blob/master/Sky.jpg');
+    var backgroundMesh = new THREE.Mesh(
+        new THREE.PlaneGeometry(2,2,0),
+        new THREE.MeshBasicMaterial({
+            map: backgroundTexture
+        }));
     
+    var backgroundScene = new THREE.Scene();
+    var backgroundCamera = new THREE.Camera();
+    backgroundScene.add(backgroundCamera);
+    backgroundScene.add(backgroundMesh);
     
     
     
